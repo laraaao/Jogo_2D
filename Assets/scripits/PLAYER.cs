ba8c58a4c7cf2ce8456355cf0f58b6dff392f926
Lara <laraduartedc@gmail.com>
2026-09-22 09:28:30 -0300

    pulo e camera
    o pulo deixou de ser infinito e colocamos a câmera para seguir o player, de forma que não fique rodando

--- Assets/scripits/PLAYER.cs
+++ Assets/scripits/PLAYER.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class PLAYER : MonoBehaviour
 {
     public float speed = 5f;
+   
+
     private Rigidbody2D rb;
+
+    private bool isGrounded = false;
+
 
     
     void Start()
@@ -17,10 +22,28 @@ public class PLAYER : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");// vai reconhecer o movimento horizontal
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y);// vai aplicar a velocidade horizontal
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); // vai gerar o pulo 
         }
-
     }
+
+     void OnCollisionEnter2D (Collision2D collision)
+     {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; // vai reconhecer quando o jogador estiver encostando no chao
+        }
+     }
+
+     void OnCollisionExit2D(Collision2D collision)
+     {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; //vai reconhecer quando o jogador nao estiver de encostando no chao
+        }
+     }
+
+
+
 }
